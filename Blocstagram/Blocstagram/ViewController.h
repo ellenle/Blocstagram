e5a7cf6d376338cdc649748c19056db0fb3d15e0
Ellen <ellenleis@gmail.com>
2016-01-19 20:57:12 -0800

    checkpoint 27 - removed storyboard

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Ellen Thuy Le on 1/19/16.
+//  Copyright (c) 2016 Ellen Thuy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
