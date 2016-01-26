34731efc81f1a88e38b8cf2b544a68e9ce1b7e57
Ellen <ellenleis@gmail.com>
2016-01-26 00:16:54 -0800

    checkpoint 28 work

---
+++ Blocstagram/Blocstagram/Comment.m
@@ -0,0 +1,14 @@
+//
+//  Comment.m
+//  Blocstagram
+//
+//  Created by Ellen Thuy Le on 1/25/16.
+//  Copyright (c) 2016 Ellen Thuy Le. All rights reserved.
+//
+
+#import "Comment.h"
+#import "User.h"
+
+@implementation Comment
+
+@end
---
+++ Blocstagram/Blocstagram/DataSource.h
@@ -0,0 +1,16 @@
+//
+//  DataSource.h
+//  Blocstagram
+//
+//  Created by Ellen Thuy Le on 1/25/16.
+//  Copyright (c) 2016 Ellen Thuy Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataSource : NSObject
+
++(instancetype) sharedInstance;
+@property (nonatomic, strong, readonly) NSArray *mediaItems;
+
+@end
