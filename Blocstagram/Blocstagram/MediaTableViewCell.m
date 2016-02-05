8b968cb713aeda6f968fca37982d845aca7b4395
Ellen <ellenleis@gmail.com>
2016-02-04 20:21:54 -0800

    finished checkopint 34

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -223,4 +223,15 @@ static NSParagraphStyle *evenParagraphStyle;
     return CGRectGetMaxY(layoutCell.commentLabel.frame);
 }
 
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    [super setHighlighted:NO animated:animated];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:NO animated:animated];
+    
+}
+
+
 @end
