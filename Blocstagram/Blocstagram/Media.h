51bea2cc76dd1c832e180b6ed83166ea58f25b92
Ellen <ellenleis@gmail.com>
2016-02-14 17:58:04 -0800

    checkpoint 37 completed

--- Blocstagram/Blocstagram/Media.h
+++ Blocstagram/Blocstagram/Media.h
@@ -6,7 +6,15 @@
 //  Copyright (c) 2016 Ellen Thuy Le. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, MediaDownloadState) {
+    MediaDownloadStateNeedsImage             = 0,
+    MediaDownloadStateDownloadInProgress     = 1,
+    MediaDownloadStateNonRecoverableError    = 2,
+    MediaDownloadStateHasImage               = 3
+};
 
 @class User;
 
@@ -18,6 +26,7 @@
 @property (nonatomic, strong) UIImage *image;
 @property (nonatomic, strong) NSString *caption;
 @property (nonatomic, strong) NSArray *comments;
+@property (nonatomic, assign) MediaDownloadState downloadState;
 
 - (instancetype) initWithDictionary:(NSDictionary *)mediaDictionary;
 
