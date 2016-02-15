88a75893276993e04e251a1d438900034e25fcb4
Ellen <ellenleis@gmail.com>
2016-02-14 18:16:44 -0800

    completed assignment 37 - changed image height constraint when image not present

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -216,7 +216,7 @@ static NSParagraphStyle *evenParagraphStyle;
         if (self.mediaItem.image.size.width > 0 && CGRectGetWidth(self.contentView.bounds) > 0) {
             self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
         } else {
-            self.imageHeightConstraint.constant = 0;
+            self.imageHeightConstraint.constant = 100;
         }
     // Hide the line between cells
     self.separatorInset = UIEdgeInsetsMake(0, CGRectGetWidth(self.bounds)/2.0, 0, CGRectGetWidth(self.bounds)/2.0);
